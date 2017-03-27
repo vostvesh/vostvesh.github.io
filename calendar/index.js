4d936dc2dc51c25841a4f9e44ffddcc8c815971e
vostvesh <vostvesh@gmail.com>
2017-03-27 15:51:14 +0300

    ...

--- calendar/index.js
+++ calendar/index.js
@@ -1,6 +1,6 @@
 let calendar1 = new Calendar('.root-calendar1', calendarDomData);
 calendar1.setDate();
 let calendar2 = new Calendar('.root-calendar2', calendarDomData);
-calendar2.setDate('2016-1-14');
+calendar2.setDate('2016-1-15');
 let calendar3 = new Calendar('.root-calendar3', calendarDomData);
 calendar3.setDate('1939-9-1');
