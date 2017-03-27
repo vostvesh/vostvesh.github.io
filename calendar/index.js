2b389b90e6b39b526e471beef989f9f2089188c9
vostvesh <vostvesh@gmail.com>
2017-03-27 15:19:24 +0300

    fixed incorrect property in calendar/index.js

--- calendar/index.js
+++ calendar/index.js
@@ -1,6 +1,6 @@
 let calendar1 = new Calendar('.root-calendar1', calendarDomData);
 calendar1.setDate();
 let calendar2 = new Calendar('.root-calendar2', calendarDomData);
-calendar2.setDate('6-14-2015');
+calendar2.setDate('2016-1-14');
 let calendar3 = new Calendar('.root-calendar3', calendarDomData);
-calendar3.setDate('9-1-1939');
+calendar3.setDate('1939-9-1');
