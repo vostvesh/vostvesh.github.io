c92a11a18ae264ac56ab4aa83f90e960119a7d8e
Aleksandr Shevtsov <vostvesh@gmail.com>
2017-03-27 15:44:03 +0300

    safari bug detected

--- calendar/index.js
+++ calendar/index.js
@@ -3,4 +3,4 @@ calendar1.setDate();
 let calendar2 = new Calendar('.root-calendar2', calendarDomData);
 calendar2.setDate('2016-1-14');
 let calendar3 = new Calendar('.root-calendar3', calendarDomData);
-calendar3.setDate('Dec 25, 1995');
+calendar3.setDate('1939-9-1');
